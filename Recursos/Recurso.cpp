e839870e828244bc98d01063190e40aef646564a
quimmilho <jrodmilheiro@gmail.com>
2021-11-20 16:58:40 +0000

    Adicionado recursos.cpp e recursos.h

---
+++ Recursos/Recurso.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#include "Recurso.h"
---
+++ Recursos/Recurso.h
@@ -0,0 +1,14 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#ifndef PROJ_FINAL_POO_RECURSO_H
+#define PROJ_FINAL_POO_RECURSO_H
+
+
+class Recurso {
+
+};
+
+
+#endif //PROJ_FINAL_POO_RECURSO_H
