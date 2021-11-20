f64738401db212d072095f16531f5003bededcca
quimmilho <jrodmilheiro@gmail.com>
2021-11-20 15:09:52 +0000

    Criado o mineiro + teste de funções hierarquizadas

---
+++ trabalhadores/Mineiro.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#include "Mineiro.h"
+
+void Trabalhador::fimDoDia() {
+
+}
---
+++ trabalhadores/Mineiro.h
@@ -0,0 +1,15 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#ifndef PROJ_FINAL_POO_MINEIRO_H
+#define PROJ_FINAL_POO_MINEIRO_H
+
+#include "Trabalhador.h"
+
+class Mineiro : public Trabalhador {
+
+};
+
+
+#endif //PROJ_FINAL_POO_MINEIRO_H
