a4900e6f935c4ebbd050fae747c7f60da8586de7
quimmilho <jrodmilheiro@gmail.com>
2021-11-20 13:53:53 +0000

    Adicionado 1 tipo de trabalhador (operario)

---
+++ trabalhadores/operario.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#include "operario.h"
+
+int k () {
+
+}
---
+++ trabalhadores/operario.h
@@ -0,0 +1,15 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#ifndef PROJ_FINAL_POO_OPERARIO_H
+#define PROJ_FINAL_POO_OPERARIO_H
+
+#include "trabalhadores.h"
+
+class operario : public trabalhadores{
+
+};
+
+
+#endif //PROJ_FINAL_POO_OPERARIO_H
