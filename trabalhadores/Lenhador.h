ffadddfc6faa5919f19e78be11d0cfbb9dfde8e7
quimmilho <jrodmilheiro@gmail.com>
2021-11-20 15:09:42 +0000

    Criado o lenhador + teste de funções hierarquizadas

---
+++ trabalhadores/Lenhador.h
@@ -0,0 +1,15 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#ifndef PROJ_FINAL_POO_LENHADOR_H
+#define PROJ_FINAL_POO_LENHADOR_H
+
+#include "Trabalhador.h"
+
+class Lenhador : public Trabalhador {
+
+};
+
+
+#endif //PROJ_FINAL_POO_LENHADOR_H
