0d55fe2e96d404da2d13a486d2b3aa39d0d89b24
quimmilho <jrodmilheiro@gmail.com>
2021-11-20 18:34:58 +0000

    Adicionadas as edificios

---
+++ edificios/Edificio.cpp
@@ -0,0 +1,2 @@
+
+#include "Edificio.h"
---
+++ edificios/Edificio.h
@@ -0,0 +1,18 @@
+
+#ifndef PROJ_FINAL_POO_EDIFICIO_H
+#define PROJ_FINAL_POO_EDIFICIO_H
+
+#define MINA_FERRO "mnF"
+#define MINA_CARVAO "mnC"
+#define CENTRAL_ELETRICA "elec"
+#define BATERIA "bat"
+#define FUNDICAO "fun"
+#define EDIFICIO_X "edfX"
+
+class Edificio {
+public:
+    void finalDoDia();
+};
+
+
+#endif //PROJ_FINAL_POO_EDIFICIO_H
