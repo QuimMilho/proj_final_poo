7543fa8b55d4f66d5326e7f827b0b9dae223dc77
quimmilho <jrodmilheiro@gmail.com>
2021-11-20 13:43:57 +0000

    Adicionados trabalhadores com as definições base (n e d) e respetivos getters

---
+++ trabalhadores/trabalhadores.cpp
@@ -0,0 +1,13 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#include "trabalhadores.h"
+
+int trabalhadores::getN() const {
+    return n;
+}
+
+int trabalhadores::getD() const {
+    return d;
+}
---
+++ trabalhadores/trabalhadores.h
@@ -0,0 +1,19 @@
+//
+// Created by jrodm on 11/20/2021.
+//
+
+#ifndef PROJ_FINAL_POO_TRABALHADORES_H
+#define PROJ_FINAL_POO_TRABALHADORES_H
+
+
+class trabalhadores {
+private:
+    int n, d;
+public:
+    int getN() const;
+
+    int getD() const;
+};
+
+
+#endif //PROJ_FINAL_POO_TRABALHADORES_H
