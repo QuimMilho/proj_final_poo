8f64696793568b0e8a900bded2595c35366181f2
quimmilho <jrodmilheiro@gmail.com>
2021-11-21 22:16:14 +0000

    Adicionado o Handler (novamente) desta vez tudo a funcionar bem, Adicionada a capacidade de o CmdExec receber o CommandHandler como parametro no constructor para futura implementação própria de comandos

--- comandos/cmds/CmdExec.cpp
+++ comandos/cmds/CmdExec.cpp
@@ -1,6 +1,8 @@
 
 #include "CmdExec.h"
 
+CmdExec::CmdExec(Handler *handler, CommandHandler *cmdHand) : Comando(handler), cmdHand(cmdHand) {}
+
 int CmdExec::execute(std::vector<std::string> *args) {
     if (args->size() == 1) {
 
--- comandos/cmds/CmdExec.h
+++ comandos/cmds/CmdExec.h
@@ -3,12 +3,18 @@
 #define PROJ_FINAL_POO_CMDEXEC_H
 
 #include "../Comando.h"
+#include "../CommandHandler.h"
 
 class CmdExec : public Comando {
+private:
+    CommandHandler *cmdHand;
+
 public:
     int execute(std::vector<std::string> *args) override;
 
     virtual ~CmdExec();
+
+    CmdExec(Handler *handler, CommandHandler *cmdHand);
 };
 
 #endif //PROJ_FINAL_POO_CMDEXEC_H
