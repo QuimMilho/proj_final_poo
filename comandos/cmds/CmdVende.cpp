19a46a598772ddb0a05191f58d978e4dd22864b2
quimmilho <jrodmilheiro@gmail.com>
2021-11-21 22:47:57 +0000

    Adicionado comando vende

--- comandos/cmds/CmdVende.cpp
+++ comandos/cmds/CmdVende.cpp
@@ -4,7 +4,7 @@
 CmdVende::CmdVende(Handler *handler) : Comando(handler) {}
 
 int CmdVende::execute(std::vector<std::string> *args)  {
-    if (args->size() == 3) {
+    if (args->size() == 2) {
 
     } else {
         std::cout << "O numero de argumentos do comando e invalido!\n";
