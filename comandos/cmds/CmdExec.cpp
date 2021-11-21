874120b2019310e7b6ac4c035ce6068cd0ae46e8
quimmilho <jrodmilheiro@gmail.com>
2021-11-21 17:31:17 +0000

    Adicionado bug onde n havia mudanças de linha

--- comandos/cmds/CmdExec.cpp
+++ comandos/cmds/CmdExec.cpp
@@ -5,7 +5,7 @@ int CmdExec::execute(std::vector<std::string> *args) {
     if (args->size() == 1) {
 
     } else {
-        std::cout << "O numero de argumentos do comando e invalido!";
+        std::cout << "O numero de argumentos do comando e invalido!\n";
     }
     return 0;
 }
