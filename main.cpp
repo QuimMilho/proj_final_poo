59284c3a5ddf6a64230fae363e696f48bfea9819
quimmilho <jrodmilheiro@gmail.com>
2021-11-21 22:27:50 +0000

    Cria o handler e passa-o para o command handler!

--- main.cpp
+++ main.cpp
@@ -3,8 +3,9 @@
 #include "comandos/CommandHandler.h"
 
 int main() {
-    CommandHandler cmdHand;
-    cmdHand.registerCmds();
+    Handler handler;
+    CommandHandler *cmdHand = new CommandHandler(&handler);
+    cmdHand->registerCmds();
     std::cout << "Bem vindo ao Industries & Co.!\n" <<
             "Para comecar cria um jogo ou carrega um a partir de um fichiro existente\n";
     bool loop = true;
@@ -12,7 +13,7 @@ int main() {
         std::string str;
         std::cout << "Insira o comando:";
         std::getline(std::cin, str);
-        cmdHand.executar(str);
+        cmdHand->executar(str);
         if (str == "exit")
             loop = false;
     }
