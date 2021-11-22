b9978bafdb6414764d6a608b2dc542d113788a91
quimmilho <jrodmilheiro@gmail.com>
2021-11-22 19:23:43 +0000

    Adicionados códigos das construções, trabalhadores, recursos e zonas

--- Handler.h
+++ Handler.h
@@ -5,6 +5,39 @@
 #include <vector>
 #include <string>
 
+//Edificios
+
+#define MINA_FERRO "mnF"
+#define MINA_CARVAO "mnC"
+#define EST_ELETRICA "elec"
+#define BATERIA "bat"
+#define FUNDICAO "fun"
+#define EDIFIDIO_X "edX"
+
+//Trabalhadores
+
+#define MINEIRO "miner"
+#define LENHADOR "len"
+#define OPERARIO "oper"
+
+//Zonas
+
+#define MONTANHA "mnt"
+#define DESERTO "dsr"
+#define PASTAGEM "pas"
+#define FLORESTA "flr"
+#define PANTANO "pnt"
+#define ZONA_X "znX"
+
+//Recursos
+
+#define FERRO "iron"
+#define BARRA_ACO "stel"
+#define CARVAO "coal"
+#define MADEIRA "wood"
+#define VIGAS_WOOD "vWod"
+#define ELETRICIDADE "what"
+
 class Handler {
 public:
     bool isNumber(std::string text);
