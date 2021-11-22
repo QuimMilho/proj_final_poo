7a0a3d56bdd06a3246ec2dd8a0d539f72cdfa816
quimmilho <jrodmilheiro@gmail.com>
2021-11-22 16:06:09 +0000

    Comando exec a funcionar a 100% :)

--- comandos/cmds/CmdExec.cpp
+++ comandos/cmds/CmdExec.cpp
@@ -1,11 +1,22 @@
 
 #include "CmdExec.h"
+#include <fstream>
 
 CmdExec::CmdExec(Handler *handler, CommandHandler *cmdHand) : Comando(handler), cmdHand(cmdHand) {}
 
 int CmdExec::execute(std::vector<std::string> *args) {
     if (args->size() == 1) {
-
+        std::string path = "cfg/" + args->at(0) + ".cfg";
+        std::ifstream file(path);
+        if (file.good()) {
+            std::string text;
+            while (getline (file, text)) {
+                cmdHand->executar(text);
+            }
+        } else {
+            std::cout << "Esse ficheiro nao existe!\n";
+        }
+        file.close();
     } else {
         std::cout << "O numero de argumentos do comando e invalido!\n";
     }
