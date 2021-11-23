0c29a8dcd16d12f8c0a14b3d0f0af46051d400cf
quimmilho <jrodmilheiro@gmail.com>
2021-11-23 12:16:28 +0000

    Adicionada função para verificar se um número é um float ou não

--- Handler.cpp
+++ Handler.cpp
@@ -6,3 +6,25 @@ bool Handler::isNumber(std::string text) {
     while (it != text.end() && std::isdigit(*it)) ++it;
     return !text.empty() && it == text.end();
 }
+
+bool Handler::isFloat(std::string text) {
+    if (text.empty()) {
+        return false;
+    } else if (isNumber(text))
+        return true;
+    else {
+        int i, ponto = 0;
+        for (i = 0; i < text.size(); i++) {
+            if (text.at(i) == '.') {
+                if (ponto != 0) {
+                    return false;
+                } else {
+                    ponto++;
+                }
+            } else if (!std::isdigit(text.at(i))) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
