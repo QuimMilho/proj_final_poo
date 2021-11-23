e65b26cbd35a20bf1146c023ad57b8154b37c7a8
quimmilho <jrodmilheiro@gmail.com>
2021-11-23 23:54:44 +0000

    Create Zone quase acabado

--- comandos/cmds/CmdCrtZn.cpp
+++ comandos/cmds/CmdCrtZn.cpp
@@ -7,7 +7,13 @@ int CmdCrtZn::execute(std::vector<std::string> *args)  {
     if (args->size() == 2) {
         if (!handler->areCoordsValid(args->at(0), args->at(1)))
             return 0;
+        int coluna = std::stoi(args->at(1)), linha = std::stoi(args->at(0));
+        handler->setColuna(coluna);
+        handler->setLinha(linha);
+        int i;
+        for (i = 0; i < coluna * linha; i++) {
 
+        }
     } else {
         std::cout << "O numero de argumentos do comando e invalido!\n";
     }
